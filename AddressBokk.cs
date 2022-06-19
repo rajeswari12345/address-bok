22d677e2f62d4424aeeafa598f0dbe9d08e6e3c3
rajeswari12345 <103126024+rajeswari12345@users.noreply.github.com>
2022-06-19 19:32:44 +0530

    AddressBokk.cs

---
+++ AddressBokk.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class AddressBook
+{
+    List<Address> addresses;
+
+    public AddressBook()
+    {
+        addresses = new List<Address>();
+    }
+
+    public bool add(string name, string address)
+    {
+        Address addr = new Address(name, address);
+        Address result = find(name);
+
+        if (result == null)
+        {
+            addresses.Add(addr);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool remove(string name)
+    {
+        Address addr = find(name);
+
+        if (addr != null)
+        {
+            addresses.Remove(addr);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void list(Action<Address> action)
+    {
+        addresses.ForEach(action);
+    }
+
+    public bool isEmpty()
+    {
+        return (addresses.Count == 0);
+    }
+
+    public Address find(string name)
+    {
+        Address addr = addresses.Find(
+          delegate (Address a) {
+              return a.name == name;
+          }
+        );
+        return addr;
+    }
+}
